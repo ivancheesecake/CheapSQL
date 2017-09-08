2772fd000e1d4d45aba006c8867b55705161ca12
Cenon Divina <xe.divina@gmail.com>
2017-09-08 12:07:11 +0800

    Basic UI, may error pa sa server

--- server.py
+++ server.py
@@ -7,10 +7,22 @@ def index():
 	
 	return render_template("index.html")
 
-@app.route('/query',methods=['POST'])
+@app.route('/query',methods=['GET','POST'])
 def query():
 
-	resp = {"success":"true"}
+	resp = {}
+
+	resp["table_name"] = "STUDENT"
+	resp["columns"] = ["StudNo","StudentName","Birthday","Degree","Major","UnitsEarned"]
+	data = []
+
+	for i in range(10):
+		row = {"2010-42113","Ivan Marc H. Escamos","1994-06-06","MS Computer Science","Forest Resource Management","0"}
+		data.append(row)
+
+	resp["data"] = data
+
+	# return "Hello"
 	return jsonify(resp)	
 	# return render_template("index.html")
 
